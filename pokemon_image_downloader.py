16545ece5b524a59d0825b16228160801319c32d
Irfan Sadek <sadekirfan3@gmail.com>
2019-10-07 22:15:52 +0600

    Add files via upload

---
+++ pokemon_image_downloader.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+
+
+src = requests.get("https://pokemondb.net/pokedex/national")
+soup = BeautifulSoup(src.content, 'lxml')
+image = soup.find_all('span', attrs = {'class':'infocard-lg-img'})
+dir = []
+for i in image:
+	for j in i.find_all('a'):
+		dir.append(j.span.get('data-src'))
+
+for i in range(1,len(dir)):
+	r = requests.get(dir[i])
+	with open('image{}.jpg'.format(i+1), 'wb') as f:
+		f.write(r.content)
